556562fc4b2f0ef9d45ac1da8d2c05477385b820
Chioma2000 <jozinnachioma@gmail.com>
2022-06-02 20:41:54 -0700

    a program that imports the variable a from the file and prints its value

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
-import sys
-
-
-def main(*argv):
-    l = len(sys.argv)
-    sum = 0
-    if l > 1:
-        for args in sys.argv:
-            if args != sys.argv[0]:
-                sum = sum + int(args)
-    print(sum)
-
 if __name__ == "__main__":
-    main()
+    from sys import argv
+    count = len(argv) - 1
+    sum = 0
+    if (count == 0):
+        print("{:d}".format(sum))
+    else:
+        for i in range(count):
+            sum += int(argv[i + 1])
+        print("{:d}".format(sum))
--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python3
-import hidden_4
-
-
-def main():
-    l = dir(hidden_4)
-    for i in range(len(l)):
-        if(l[i][0] != '_'):
-            print("{}".format(l[i]))
-
 if __name__ == "__main__":
-    main()
+    import hidden_4
+    for i in dir(hidden_4):
+        if (i[:2] != "__"):
+            print(i)
