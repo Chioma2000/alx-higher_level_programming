efc74107f729575cd171a181091159f048190c6a
Chioma2000 <jozinnachioma@gmail.com>
2022-07-05 10:13:49 -0700

    Task-8 Update

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
-"""
-eturns the dictionary description with simple data structure
-(list, dictionary, string, integer and boolean)
-for JSON serialization of an object
+""" Module that returns the dictionary description with a simple
+data structure for a JSON serialization of an object
 """
 
 
 def class_to_json(obj):
-    """
-    eturns the dictionary description with simple data structure
-    (list, dictionary, string, integer and boolean)
-    for JSON serialization of an object
-    """
-    return(obj.__dict__) 
+    """ Function that retuns the dictionary description of an obj """
+
+    res = {}
+    if hasattr(obj, "__dict__"):
+        res = obj.__dict__.copy()
+    return res 
