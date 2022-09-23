b25a8669250ea2479b4e57e6397f9c46426285ae
Chioma2000 <jozinnachioma@gmail.com>
2022-09-23 13:18:09 -0700

    updating task errors

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-"""A script that
-- fetches https://alx-intranet.hbtn.io/status.
-- uses urlib package
+# -*- coding: utf-8 -*-
 """
+Created on Mon Aug 24 05:14:59 2020
+@author: Robinson Montes
+"""
+from urllib.request import urlopen
 
 
-if __name__ == '__main__':
-    import urllib.request
-
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
-        content = res.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(content.decode('utf-8')))
+if __name__ == "__main__":
+    url = 'https://intranet.hbtn.io/status'
+    with urlopen(url) as response:
+        bytes_content = response.read()
+        content = bytes_content.decode('utf-8')
+        string = 'Body response:\n\t- type: {}\n\t- content: {}\n\t- \
+utf8 content: {}'.format(type(bytes_content), bytes_content, content)
+        print(string)
